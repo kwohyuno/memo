16d4c2c1c525a84cc30208f92b42367975034cab
Hyun Oh <you@example.com>
2024-05-14 14:30:19 +0900

    init commit

---
+++ src/main/java/com/sparta/memo/MemoApplication.java
@@ -0,0 +1,15 @@
+package com.sparta.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoApplication.class, args);
+    }
+
+}
+
+
